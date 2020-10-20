b02762c4a7dbfb5df71c6bdc64f72cabb996ddcc
DriesDD <driesdedecker@gmail.com>
2020-10-20 16:22:02 +0200

    basic layout

---
+++ build/precache-manifest.e44062361b59f6aa77f7edd7a9aaf678.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04e43e5e90cf6dd5281d9395632e788c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8be7ac423c959115edff",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6335517fe5e9940139b0",
+    "url": "/static/js/2.cc51fc18.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.cc51fc18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8be7ac423c959115edff",
+    "url": "/static/js/main.50a2f88f.chunk.js"
+  },
+  {
+    "revision": "25df17f3fb173fbd21e1",
+    "url": "/static/js/runtime-main.377f297c.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          My todo app
+        </h1>
       </header>
+      <form>
+        <p>Add a new todo:</p>
+        <input
+          type="text"
+        />
+      </form>
+      <h2>
+          My todos:
+        </h2>
+        <div className="List">
+      <Itemlist items={items} />
+      </div>
     </div>
   );
 }
+
+function Itemlist(props) {
+  const items = props.items;
+  const listItems = items.map((item) =>
+    <ul key={item.toString()}>
+      <span>☐</span> {item}
+    </ul>
+  );
+  return (
+    <ul>{listItems}</ul>
+  );
+}
+
+const items = ['Feed the chickens','Water the plants','Fix the doorbell'];
 
 export default App;
